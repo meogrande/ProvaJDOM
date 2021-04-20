faa25b6cc9c1cb6358a9c36c4740048212e81e31
Fabio Biscaro <fabio.biscaro@gmail.com>
2021-04-20 18:52:56 +0200

    Prova commit

---
+++ provaJDOM/src/provaJDOM/Prova.java
@@ -0,0 +1,44 @@
+package provaJDOM;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.DOMBuilder;
+import org.jdom2.input.SAXBuilder;
+
+public class Prova {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SAXBuilder builder = new SAXBuilder();
+		  File xmlFile = new File("c:\\tools\\file.xml");
+
+		  try {
+
+			Document document = (Document) builder.build(xmlFile);
+			Element rootNode = document.getRootElement();
+			List list = rootNode.getChildren("staff");
+
+			for (int i = 0; i < list.size(); i++) {
+
+			   Element node = (Element) list.get(i);
+
+			   System.out.println("First Name : " + node.getChildText("firstname"));
+			   System.out.println("Last Name : " + node.getChildText("lastname"));
+			   System.out.println("Nick Name : " + node.getChildText("nickname"));
+			   System.out.println("Salary : " + node.getChildText("salary"));
+
+			}
+
+		  } catch (IOException io) {
+			System.out.println(io.getMessage());
+		  } catch (JDOMException jdomex) {
+			System.out.println(jdomex.getMessage());
+		  }
+	}
+
+}
